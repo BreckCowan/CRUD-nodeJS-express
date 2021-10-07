f9982892af204a2f4fcbffdb18b3ad09e4aaae26
Breck Cowan <breckcowan@gmail.com>
2021-10-06 19:04:41 -0500

    connected to mongodb database natours to the app

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+dotenv.config({ path: './config.env' });
 
-// const dotenv = require('dotenv');
-// const app = require('./app');
-//
-// dotenv.config({ path: './config.env' });
-//
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}...`);
-// });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connections successful'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
